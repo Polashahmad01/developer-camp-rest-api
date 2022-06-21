3d68e61b7edd9e87fb79f5e9905c07b832ea281a
Polashahmad01 <dev.polashahmad@gmail.com>
2022-06-21 10:53:38 +0600

    Install colors package & improve dev logging console

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 
 const connectDB = require("./config/db");
 
@@ -26,12 +27,14 @@ app.use("/api/v1/bootcamps", bootcampsRouter);
 const PORT = process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
 });
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (error, promise) => {
-  console.log(`Error: ${error.message}`);
+  console.log(`Error: ${error.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
