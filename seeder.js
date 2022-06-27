34f584e9291a76e1dc709fa06ec3bfb7e068307b
Polashahmad01 <dev.polashahmad@gmail.com>
2022-06-27 21:31:16 +0600

    add database seeder

---
+++ seeder.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars 
+dotenv.config({ path: './config/config.env'});
+
+// Load Models
+const Bootcamp = require('./model/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI);
+
+// Read JSON files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data imported...'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data Destroyed...'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+if(process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
