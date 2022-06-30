28d0c65c7d9b320a9b0432120dbf373f77107dfd
Polashahmad01 <dev.polashahmad@gmail.com>
2022-06-30 11:39:24 +0600

    only admin can create more than 1 bootcamps

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env'});
 // Load Models
 const Bootcamp = require('./model/Bootcamp');
 const Course = require('./model/Course');
+const User = require('./model/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -16,12 +17,14 @@ mongoose.connect(process.env.MONGO_URI);
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8"));
 
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
 
         console.log('Data imported...'.green.inverse);
         process.exit();
@@ -35,6 +38,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
 
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
