eeb7f1cdcb5daee6b24472771b7061f46faed540
Polashahmad01 <dev.polashahmad@gmail.com>
2022-06-30 21:11:05 +0600

    update & delete review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { getReviews, getReview, addReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview, updateReview, deleteReview } = require('../controllers/reviews');
 
 const Review = require("../model/Review");
 const advancedResults = require("../middleware/advancedResults");
@@ -15,7 +15,9 @@ router
         .post(protect, authorize('user', 'admin'), addReview);
 
 router.route("/:id")
-        .get(getReview);
+        .get(getReview)
+        .put(protect, authorize('user', 'admin'), updateReview)
+        .delete(protect, authorize('user', 'admin'), deleteReview);
 
 
 module.exports = router;
